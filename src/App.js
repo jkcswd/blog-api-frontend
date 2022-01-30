1640245074afae69027bbf00f69d644e216e6c45
James Spencer <jameskcs1254@gmail.com>
2022-01-30 10:48:12 +0000

    Add hooks for storing api data in state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() =>{
+    fetchProducts();
+  },[]);
+
+  const fetchProducts = async () => {
+    const response = await fetch('http://localhost:3000/api/post', {mode:'cors'});
+    const posts = await response.json();
+
+    setPosts(posts);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
