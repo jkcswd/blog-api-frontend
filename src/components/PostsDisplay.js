033f28c659819d1c9fc3b0dec601e08acdb670e6
James Spencer <jameskcs1254@gmail.com>
2022-01-31 09:33:05 +0000

    Add key fix api route

--- src/components/PostsDisplay.js
+++ src/components/PostsDisplay.js
@@ -9,7 +9,7 @@ const PostsDisplay = () => {
   },[]);
 
   const fetchPosts = async () => {
-    const response = await fetch('http://localhost:3000/api/post', {mode:'cors'});
+    const response = await fetch('http://localhost:8000/api/post', {mode:'cors'});
     const posts = await response.json();
 
     setPosts(posts);
@@ -20,7 +20,7 @@ const PostsDisplay = () => {
       {posts.map(post => {
         if(post.isPublished) {
           return(
-            <Link to={'/' + post._id}>
+            <Link key={post._id} to={'/' + post._id}>
               <div className="blog-post">
                 <h1>{post.title}</h1>
                 <p>{'By ' + post.user.firstName + ' ' + post.user.lastName}</p>
