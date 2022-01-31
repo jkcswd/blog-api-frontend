1de4bbd397d1e7f7fa20f04c5981fd30027de62b
James Spencer <jameskcs1254@gmail.com>
2022-01-31 10:32:58 +0000

    Move api function assignments into useEffect

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,52 +1,59 @@
-import { useEffect, useState } from "react";
+import { useEffect,useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const PostDetail = () => {
-  const [post, setPost] = useState({});
   const location = useLocation();
+
+  const [post, setPost] = useState({});
   const [comment, setComment] = useState('');
+  const [commentToSend, setCommentToSend] = useState('')
   const [commentArray, setCommentArray] = useState([]);
 
   useEffect(() =>{
+    
+    const sendCommentToApi = async () => {
+      if(commentToSend) {
+        await fetch('http://localhost:8000/api/comment/', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(
+            {
+              comment: commentToSend,
+              post: location.pathname.substring(1), // Remove '/' from pathname to return post id only
+              user:'61f70a6a6efc017bbd591282'
+            }
+          )
+        });
+      } 
+  
+      setComment(''); 
+    }
+    sendCommentToApi();
+
+    const fetchPost = async () => {
+      const response = await fetch(`http://localhost:8000/api/post/${location.pathname}`, {mode:'cors'});
+      const post = await response.json();
+  
+      setPost(post);
+    }
     fetchPost();
+
+    const fetchComments = async () => {
+      const response = await fetch(`http://localhost:8000/api/post/${location.pathname}/comments`, {mode:'cors'});
+      const comments = await response.json();
+  
+      setCommentArray(comments);
+    }
     fetchComments();
-  }, []);
+  }, [commentToSend, location.pathname]);
 
-  const fetchPost = async () => {
-    const response = await fetch(`http://localhost:8000/api/post/${location.pathname}`, {mode:'cors'});
-    const post = await response.json();
-
-    setPost(post);
-  }
-
-  const fetchComments = async () => {
-    const response = await fetch(`http://localhost:8000/api/post/${location.pathname}/comments`, {mode:'cors'});
-    const comments = await response.json();
-
-    setCommentArray(comments);
-  }
-
-  const sendCommentToApi = async (e) => {
+  const updateCommentToSend = (e) => {
     e.preventDefault(); //prevent page refresh on click
 
-    if(comment) {
-      await fetch('http://localhost:8000/api/comment/', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-          {
-            comment,
-            post: location.pathname.substring(1), // Remove '/' from pathname to return post id only
-            user:'61f70a6a6efc017bbd591282'
-          }
-        )
-      });
-    } 
-
-    setComment(''); 
+    setCommentToSend(e.target.elements[0].value)
   }
 
   const handleComment = (e) => {
@@ -60,7 +67,7 @@ const PostDetail = () => {
       <h1>{post.title}</h1>
       <p>{post.text}</p>
       <h2>Comments</h2>
-      <form onSubmit={sendCommentToApi}>
+      <form onSubmit={updateCommentToSend}>
         <input type="text" value={comment} onChange={handleComment}/>
         <input type="submit" />
       </form>
