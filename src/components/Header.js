c8ed5933b1ed1b0ded1dace752be89fe923fb898
James Spencer <jameskcs1254@gmail.com>
2022-02-08 14:29:39 +0000

    Add imports, edit JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+import '../styles/Header.css'
+
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className='title'>Blog</h1>
+      <Link to="/">
+        <h1 className='title'>BLOG</h1> 
+      </Link>
     </div>
   )
 }
